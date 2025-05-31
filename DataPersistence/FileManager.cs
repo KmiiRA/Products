5045a0d9353aa04ffa306e9f2df787c4ed5fbb27
Camila Rojas <crojasa@ucenfotec.ac.cr>
2025-05-31 10:36:35 -0600

    Ultima modificación

--- DataPersistence/FileManager.cs
+++ DataPersistence/FileManager.cs
@@ -12,7 +12,7 @@ namespace DataPersistence
         //Metodo retorna una lista de productos tomada del archivo de texto.
         public List<Product> ReadProcuts()
         {
-            private string filePath = "productos.txt";
+            private string filePath = "productos.txt";¿
 
         //Desarrollar por el estudiante.
         //Leer la lista de productos de un txt y cargarlo en una lista.
@@ -20,22 +20,23 @@ namespace DataPersistence
         public List<Product> ReadProcuts()
         {
             var products = new List<Product>();
-            if (File.Exists(filePath))
+            if (File.Exists(_filePath))
             {
-                var lines = File.ReadAllLines(filePath);
+                var lines = File.ReadAllLines(_filePath);
+
                 foreach (var line in lines)
                 {
                     var parts = line.Split(',');
-                    if (parts.Length == 3)
-                    {
-                        var product = new Product();
-                        product.Id = int.Parse(parts[0]);
-                        product.Name = parts[1];
-                        product.Price = float.Parse(parts[2]);
-                        products.Add(product);
-                    }
+                    var product = new Product();
+                    product.Id = Int32.Parse(parts[0]);
+                    product.Name = parts[1];
+                    product.Price = Double.Parse(parts[2]);
+                    product.Stock = Int32.Parse(parts[3]);
+
+                    products.Add(product);
                 }
             }
+
             return products;
         }
 
@@ -48,9 +49,8 @@ namespace DataPersistence
             //Desarrollar por el estudiante.
             //Guardar el producto en un txt.
 
-            var line = $"{product.Id},{product.Name},{product.Price}";
-            File.AppendAllText(filePath, line + Environment.NewLine);
-
+            var line = product.Id + "," + product.Name + "," + product.Price + "," + product.Stock + "\n";
+            File.AppendAllText(_filePath, line);
         }
 
     }
